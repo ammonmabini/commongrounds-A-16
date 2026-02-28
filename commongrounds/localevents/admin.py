8d870dd9fefccd7c0575f6bb20cfe97c51af72a8
Ammon Mabini <ramon.jr.mabini@student.ateneo.edu>
2026-03-01 02:57:26 +0800

    added admin features

---
+++ commongrounds/localevents/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from .models import Event, EventType
+
+class EventAdmin(admin.ModelAdmin):
+    model = Event
+    search_fields = ('title', 'location')
+    list_display = ('title', 'category', 'start_time', 'end_time')
+    list_filter = ('category', 'start_time')
+    fieldsets = [
+        ('Details', {
+            'fields': [
+                ('title', 'category'),
+                'description',
+                'location',
+                ('start_time', 'end_time'),
+            ]
+        }),
+    ]
+
+admin.site.register(EventType)
+admin.site.register(Event, EventAdmin)
