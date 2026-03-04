f18f2ca681aaa8e68be21448090551a3752495ac
Femi Edu <olufemi.edu@student.ateneo.edu>
2026-03-04 18:15:54 +0800

    added diyproject urls

--- diyprojects/urls.py
+++ diyprojects/urls.py
@@ -1,1 +1,9 @@
+from django.urls import path
+from .views import project_list, project_detail
 
+app_name = 'diyprojects'
+
+urlpatterns = [
+    path('projects', project_list, name='project-list'),
+    path('project/<int:pk>', project_detail, name='project-detail'),
+]
