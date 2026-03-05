10b9161486d40e31befeaa53b14b0ad5ea23d44e
Ammon Mabini <ramon.jr.mabini@student.ateneo.edu>
2026-03-06 00:09:25 +0800

    merge-ready fixes

--- commongrounds/settings.py
+++ commongrounds/settings.py
@@ -41,9 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bookclub',
-    'merchstore',
-    'localevents',  
+    'localevents',
 ]
 
 MIDDLEWARE = [
--- merchstore/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('items', views.item_list, name='item_list'),
-    path('item/<int:product_id>', views.item_detail, name='item_detail'),
-]
